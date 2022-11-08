e63daf5972533a1a3a1e395347f7d2b9b0444c41
luc wastiaux <luc.wastiaux@airpost.net>
2022-11-08 21:37:29 +0800

    build pickle file

--- tools/build_data.py
+++ tools/build_data.py
@@ -1,10 +1,14 @@
 import os
 import sys
+import pickle
+import logging
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+logger = logging.getLogger(__file__)
 
 import pinyin_jyutping.data
 import pinyin_jyutping.parser
+import pinyin_jyutping.constants
 
 data = pinyin_jyutping.data.Data()
 
@@ -12,5 +16,9 @@ data = pinyin_jyutping.data.Data()
 filename = 'source_data/cedict_1_0_ts_utf-8_mdbg.txt'
 pinyin_jyutping.parser.parse_cedict(filename, data)            
 
+pickle_file_path = f'pinyin_jyutping/{pinyin_jyutping.constants.PICKLE_DATA_FILENAME}'
+data_file = open(pickle_file_path, 'wb')
+pickle.dump(data, data_file)
+data_file.close()
 
-
+logger.info(f'wrote {pickle_file_path}')
