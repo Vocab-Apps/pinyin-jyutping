6981326da493c46c7c57dbf1e1dfbe364159d8b6
luc wastiaux <luc.wastiaux@airpost.net>
2022-11-12 14:04:05 +0800

    test various conversion modes

--- test_pinyin_conversion.py
+++ test_pinyin_conversion.py
@@ -19,3 +19,5 @@ class PinyinConversion(unittest.TestCase):
     # @pytest.mark.skip(reason="too many alternatives")
     def test_pinyin_sentences(self):
         self.assertEqual(self.pinyin_jyutping.pinyin('忘拿一些东西了')[0], 'wàng ná yīxiē dōngxī le')
+        self.assertEqual(self.pinyin_jyutping.pinyin('忘拿一些东西了', tone_numbers=True)[0], 'wang4 na2 yi1xie1 dong1xi1 le5')
+        self.assertEqual(self.pinyin_jyutping.pinyin('忘拿一些东西了', tone_numbers=True, spaces=True)[0], 'wang4 na2 yi1 xie1 dong1 xi1 le5')
