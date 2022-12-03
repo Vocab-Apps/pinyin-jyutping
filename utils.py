eebd4c3650a4f04913271281affdd2c1b25876ad
luc wastiaux <luc.wastiaux@airpost.net>
2022-12-03 09:02:47 +0800

    cleanup

--- utils.py
+++
@@ -1,7 +0,0 @@
-import pinyin_jyutping_sentence
-
-def build_file_cache():
-    pinyin_jyutping_sentence.romanization_conversion.conversion_data.serialize()
-
-if __name__ == '__main__':
-    build_file_cache()
