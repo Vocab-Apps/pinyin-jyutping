a55dc464011cda876f53dda9f232b3ef5faf18d3
luc wastiaux <luc.wastiaux@airpost.net>
2022-12-10 19:57:14 +0800

    commit version upgrade

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@ VERSION_NUMBER=$1 # for example 0.1
 GIT_TAG=v${VERSION_NUMBER}
 
 sed -i "s/version='.*',/version='${VERSION_NUMBER}',/g" setup.py
+git commit -m "version upgrade to ${VERSION_NUMBER}" setup.py
 
 git push
 git tag -a ${GIT_TAG} -m "version ${GIT_TAG}"
